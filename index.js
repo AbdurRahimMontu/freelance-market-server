a5c9df67faef428261dd1aa336f576d6409fb862
AbdurRahimMontu <abdurrahim9977@gmail.com>
2025-11-11 05:26:49 +0600

    allJobs api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,6 +11,53 @@ app.use(express.json())
 app.get('/', (req,res)=>{
     res.send('Server is running')
 })
+
+
+
+const uri = "mongodb+srv://freelanceDB:Y6EV6vWqWHntom8S@cluster0.juobova.mongodb.net/?appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+   
+    const db = client.db('freelancerDB')
+    const freelancer = db.collection('freelancers')
+
+ app.get('/allJobs', async (req,res)=>{
+    const result = await freelancer.find().toArray()
+    res.send(result)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
