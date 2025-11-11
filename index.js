d4c9317081b99b6380835787acfce0b29d90927d
AbdurRahimMontu <abdurrahim9977@gmail.com>
2025-11-11 07:13:12 +0600

    job details page add.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -35,6 +35,13 @@ async function run() {
 
  app.get('/allJobs', async (req,res)=>{
     const result = await freelancer.find().toArray()
+    res.send(result)
+})
+
+ app.get('/allJobs/:id', async (req,res)=>{
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await freelancer.findOne(query)
     res.send(result)
 })
 
