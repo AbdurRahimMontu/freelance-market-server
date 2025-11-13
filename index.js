8a9fee927a4028b8b257c60127e48d249c57d345
AbdurRahimMontu <abdurrahim9977@gmail.com>
2025-11-13 13:39:09 +0600

    comment db

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db('freelancerDB')
     const freelancer = db.collection('freelancers')
 
@@ -105,7 +105,7 @@ app.delete("/myAcceptedTasks/:id", async (req, res) => {
 });
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
 
